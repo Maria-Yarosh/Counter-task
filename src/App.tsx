71c29834ede35ec96b487dbf71df508c4c81056b
Maria <masya757575@yandex.ru>
2026-05-11 11:27:40 +0300

    update settings functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,94 @@
-import { useState } from 'react'
-import './App.css'
-import { Button } from './Button'
+import { useEffect, useState, type ChangeEvent } from "react";
+import "./App.css";
+import { Button } from "./Button";
 
 function App() {
-  const [count, setCount] = useState<number>(0)
+  const [count, setCount] = useState<number>(0);
+  const [maxValue, setMaxValue] = useState(() => {
+    const valueFromLocalStorage = localStorage.getItem("maxValue");
+
+    return valueFromLocalStorage !== null ? Number(valueFromLocalStorage) : 0;
+  });
+  const [startValue, setStartValue] = useState(() => {
+    const valueFromLocalStorage = localStorage.getItem("startValue");
+
+    return valueFromLocalStorage !== null ? Number(valueFromLocalStorage) : 0;
+  });
+
+  useEffect(() => {
+    localStorage.setItem("maxValue", JSON.stringify(maxValue));
+  }, [maxValue]);
+
+  useEffect(() => {
+    localStorage.setItem("startValue", JSON.stringify(startValue));
+  }, [startValue]);
 
   const handleIncreaseBtnClick = () => {
-    if(count < 5) {
-      setCount(count + 1)
+    if (count < maxValue) {
+      setCount(count + 1);
     }
-  }
+  };
 
   const handleResetBtnClick = () => {
-    setCount(0)
-  }
+    setCount(startValue);
+  };
+
+  const handleMaxValueChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setMaxValue(+e.currentTarget.value);
+  };
+
+  const handleStartValueChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setStartValue(+e.currentTarget.value);
+  };
+
+  const handleSetClick = () => {
+    setCount(startValue);
+  };
 
   return (
-    <div className='container'>
-      <div className='counter'>
-        <p className={count === 5 ? 'error-count' : ''}>{count}</p>
+    <div>
+      <div className="container">
+        <div>
+          <span>
+            <p> max value: </p>
+            <input
+              type="number"
+              value={maxValue}
+              onChange={handleMaxValueChange}
+            />
+          </span>
+          <span>
+            <p>start value: </p>
+            <input
+              type="number"
+              value={startValue}
+              onChange={handleStartValueChange}
+            />
+          </span>
+        </div>
+        <div className="btn-container">
+          <Button title="set" onClick={handleSetClick} disabled={false} />
+        </div>
       </div>
-      <div className='btn-container'>
-        <Button title='inc' onClick={handleIncreaseBtnClick} disabled={count === 5 }/>
-        <Button title='reset' onClick={handleResetBtnClick} disabled={count === 0 || count < 5}/>
+      <div className="container">
+        <div className="counter">
+          <p className={count === maxValue ? "error-count" : ""}>{count}</p>
+        </div>
+        <div className="btn-container">
+          <Button
+            title="inc"
+            onClick={handleIncreaseBtnClick}
+            disabled={count === maxValue}
+          />
+          <Button
+            title="reset"
+            onClick={handleResetBtnClick}
+            disabled={count === 0 || count < maxValue}
+          />
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
